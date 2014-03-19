133605433deb0e333662dd16f422457552eaa173
Martin Kersner <m.kersner@gmail.com>
2014-03-19 22:36:11 +0900

    used functions to call parsers for each section

--- homepage_scrap.py
+++ homepage_scrap.py
@@ -12,51 +12,88 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+from parse import *
+
+# SETTINGS
+sep = "\t"
 
 ## downloading main page
 response = requests.get("http://www.greenbook.org/")
 html = response.text
 
-s = BeautifulSoup(html)
+bs = BeautifulSoup(html)
 
 ## BROWSE BY MARKET RESEARCH SPECIALTIES #######################################
-## Market Research Specialities
+## MARKET RESEARCH SPECIALTIES
+head_mrsid = ["url", "category", "subcategory"]
+
+## Buisness Issues
+wr_mrsid1 = open('MRSID_1.csv', 'w')
+parse_mrsid(bs, "MRSID_1", wr_mrsid1, head_mrsid, sep)
+wr_mrsid1.close
+
+## Research Solutions
+wr_mrsid2 = open('MRSID_2.csv', 'w')
+parse_mrsid(bs, "MRSID_2", wr_mrsid2, head_mrsid, sep)
+wr_mrsid2.close
+
+## Research Services
+wr_mrsid3 = open('MRSID_3.csv', 'w')
+parse_mrsid(bs, "MRSID_3", wr_mrsid3, head_mrsid, sep)
+wr_mrsid3.close
+
+## Industries & Demographics
+wr_mrsid4 = open('MRSID_4.csv', 'w')
+parse_mrsid(bs, "MRSID_4", wr_mrsid4, head_mrsid, sep)
+wr_mrsid4.close
+
+## Related Services & Software
+wr_mrsid5 = open('MRSID_5.csv', 'w')
+parse_mrsid(bs, "MRSID_5", wr_mrsid5, head_mrsid, sep)
+wr_mrsid5.close
+
+## International Expertise
+wr_mrsid6 = open('MRSID_6.csv', 'w')
+parse_mrsid(bs, "MRSID_6", wr_mrsid6, head_mrsid, sep)
+wr_mrsid6.close
 
 ## BROWSE BY LOCATION ##########################################################
-## MRS Market Reaserch Firms
-## countries, states, metro area
-writer_ctry = open('MRS_CTRY.csv','w')
-writer_state = open('MRS_STATE.csv','w')
-writer_metro = open('MRS_METRO.csv','w')
+### MRS ALL MARKET RESEARCH FIRMS
 
-for link in s.find(id="browse_loc_MRS_CTRY").find_all("a"):
-  writer_ctry.write(link.get("href") + "\t" + link.string + "\n")
+## Countries
+head_ctry = ["url", "country"]
+wr_ctry = open('MRS_CTRY.csv', 'w')
+parse_loc(bs, "browse_loc_MRS_CTRY", wr_ctry, head_ctry, sep)
+wr_ctry.close
 
-for link in s.find(id="browse_loc_MRS_STATE").find_all("a"):
-  writer_state.write(link.get("href") + "\t" + link.string + "\n")
+## States
+head_state = ["url", "state"]
+wr_state = open('MRS_STATE.csv', 'w')
+parse_loc(bs, "browse_loc_MRS_STATE", wr_state, head_state, sep)
+wr_state.close
 
-for link in s.find(id="browse_loc_MRS_METRO").find_all("a"):
-  writer_metro.write(link.get("href") + "\t" + link.string + "\n")
+## Metro area
+head_metro = ["url", "metro_area"]
+wr_metro = open('MRS_METRO.csv', 'w')
+parse_loc(bs, "browse_loc_MRS_METRO", wr_metro, head_metro, sep)
+wr_metro.close
 
-writer_ctry.close;
-writer_state.close;
-writer_metro.close;
+### FCG FOCUS GROUP FACILITIES
 
-## FCG Focus Group Facilities
-## countries, states, metro area
-writer_fcg_ctry = open('FCG_CTRY.csv','w')
-writer_fcg_state = open('FCG_STATE.csv','w')
-writer_fcg_metro = open('FCG_METRO.csv','w')
+## Countries, States, Metro area
+head_ctry = ["url", "country"]
+wr_fcg_ctry = open('FCG_CTRY.csv','w')
+parse_loc(bs, "browse_loc_FCG_CTRY", wr_fcg_ctry, head_ctry, sep)
+wr_fcg_ctry.close
 
-for link in s.find(id="browse_loc_FCG_CTRY").find_all("a"):
-  writer_fcg_ctry.write(link.get("href") + "\t" + link.string + "\n")
+## State
+head_state = ["url", "state"]
+wr_fcg_state = open('FCG_STATE.csv','w')
+parse_loc(bs, "browse_loc_FCG_STATE", wr_fcg_state, head_state, sep)
+wr_fcg_state.close
 
-for link in s.find(id="browse_loc_FCG_STATE").find_all("a"):
-  writer_fcg_state.write(link.get("href") + "\t" + link.string + "\n")
-
-for link in s.find(id="browse_loc_FCG_METRO").find_all("a"):
-  writer_fcg_metro.write(link.get("href") + "\t" + link.string + "\n")
-
-writer_fcg_ctry.close;
-writer_fcg_state.close;
-writer_fcg_metro.close;
+## Metro area
+head_metro = ["url", "metro_area"]
+wr_fcg_metro = open('FCG_METRO.csv','w')
+parse_loc(bs, "browse_loc_FCG_METRO", wr_fcg_metro, head_metro, sep)
+wr_fcg_metro.close
